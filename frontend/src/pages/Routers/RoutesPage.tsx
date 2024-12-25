c8b7972224c051619a87a07f12070448316f7e8f
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2024-12-25 21:18:46 +0100

    Filters added to routes

--- frontend/src/pages/Routers/RoutesPage.tsx
+++ frontend/src/pages/Routers/RoutesPage.tsx
@@ -1,12 +1,17 @@
 // React - imports:
 import { useEffect, useState } from 'react';
 
+// Ant designe - Form import:
+import type { SelectProps } from 'antd';
+import { Slider, Select } from 'antd';
+
 // Application - Components import:
 import RouteCardComponent from '../../components/Objects/Routes/RouteCardComponent';
 
 // Application - Icons import:
 import DisplayTableIcon from '../../assets/icons/interface/DisplayTableIcon';
 import DisplayCardIcon from '../../assets/icons/interface/DisplayCardIcon';
+import EmptyResponse from '../../assets/icons/interface/EmptyResponseIcon';
 import FiltersIcon from '../../assets/icons/interface/FiltersIcon';
 
 // Application - CSS imports:
@@ -29,10 +34,17 @@ interface Routes {
 
 export default function RoutesList() {
   
+  // Constance values:
   const [routes, setRoutes] = useState<Routes[]>([]);
+  const [filteredRoutes, setFilteredRoutes] = useState<Routes[]>([]);
+  const [distanceFilter, setDistanceFilter] = useState<[number, number]>([0, 100]);
+  const [durationFilter, setDurationFilter] = useState<[number, number]>([0, 24]);
+  const [elevationFilter, setElevationFilter] = useState<[number, number]>([0, 3000]);
+  const [routeTypeFilter, setRouteTypeFilter] = useState<string | null>(null);
+  const [showFilters, setShowFilters] = useState(false);
 
+  // Collect routes from json file:
   useEffect(() => {
-    // Fetch the data when the component mounts:
     fetch('/routes.json')
       .then((response) => {
         if (!response.ok) {
@@ -45,6 +57,27 @@ export default function RoutesList() {
       })
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
+
+  // Filters - Slider:
+  const toggleFilters = () => {
+    setShowFilters((prev) => !prev);
+  };
+
+  // Update the filtered routes whenever filters change
+  useEffect(() => {
+    const filtered = routes.filter((route) => {
+      return (
+        route.distance >= distanceFilter[0] &&
+        route.distance <= distanceFilter[1] &&
+        route.duration >= durationFilter[0] &&
+        route.duration <= durationFilter[1] &&
+        route.elevation_gain >= elevationFilter[0] &&
+        route.elevation_gain <= elevationFilter[1] &&
+        (routeTypeFilter ? route.type === routeTypeFilter : true)
+      );
+    });
+    setFilteredRoutes(filtered);
+  }, [routes, distanceFilter, durationFilter, elevationFilter, routeTypeFilter]);
 
   return (
     <>
@@ -59,13 +92,66 @@ export default function RoutesList() {
                 <button type="button" className="card-search-button">Search</button>
             </div>
             <div className="card-section card-icons-right">
-              <div className="card-search-icon"><FiltersIcon></FiltersIcon></div>
+              <div className="card-search-icon" onClick={toggleFilters}><FiltersIcon></FiltersIcon></div>
+            </div>
+        </article>
+
+        <article className={`card card-filters card-padding ${showFilters ? '' : 'non-display'}`}>
+            <div className="card-section card-filter">
+                <div className="card-filter-slider">
+                    <label>Distance (Km):</label>
+                    <Slider
+                      min={0}
+                      max={100}
+                      step={5}
+                      range
+                      defaultValue={[0, 100]}
+                      onChange={(value) => setDistanceFilter(value)}
+                    />
+                </div>
+                <div className="card-filter-slider">
+                    <label>Duration (h):</label>
+                    <Slider
+                      min={0}
+                      max={24}
+                      step={1}
+                      range defaultValue={[0, 24]}
+                      onChange={(value) => setDurationFilter(value)}
+                    />
+                </div>
+                <div className="card-filter-slider">
+                    <label>Gain Altitude (m):</label>
+                    <Slider
+                      min={0}
+                      max={3000}
+                      step={100}
+                      range defaultValue={[0, 3000]}
+                      onChange={(value) => setElevationFilter(value)}
+                    />
+                </div>
+            </div>
+            <div className="card-section card-filter">
+              <div className="card-filter-slider">
+                <label>Route type:</label>
+                <Select
+                  placeholder="Choose route type"
+                  style={{ width: '100%' }}
+                  options={[
+                    { label: 'Hiking', value: 'hiking' },
+                    { label: 'Biking', value: 'biking' },
+                    { label: 'Running', value: 'running' },
+                  ]}
+                  onChange={(value) => setRouteTypeFilter(value || null)}
+                  allowClear
+                />
+              </div>
             </div>
         </article>
       </section>
 
       <section className="card-container-grid grid-mediun">
-        {routes.map((route) => (
+      {filteredRoutes.length > 0 ? (
+        filteredRoutes.map((route) => (
           <RouteCardComponent
             key={route.id}
             routeId={route.id}
@@ -80,7 +166,10 @@ export default function RoutesList() {
             bestSeasons={route.best_seasons}
             image={route.image}
           />
-        ))}
+        ))
+      ) : (
+        <EmptyResponse />
+      )}
       </section>
     </>
   );
